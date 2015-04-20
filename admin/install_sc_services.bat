95a6f153ffebd1d41e1727d306993d2c1f58c28d
Kuo-Wan Lin <klin@usgs.gov>
2015-04-20 09:02:20 -0600

    install sc rssd polld as manual service

--- admin/install_sc_services.bat
+++ admin/install_sc_services.bat
@@ -2,9 +2,12 @@
 
 set D=..\sc\bin
 
-rem perl %D%\polld.pl --install --sname=sc_polld
+perl %D%\polld.pl --install --sname=sc_polld
 perl %D%\dispd.pl --install --sname=sc_dispd
 perl %D%\notifyqueue.pl --install --sname=sc_notifyqueue
 perl %D%\notify.pl --install --sname=sc_notify
-rem perl %D%\rssd.pl --install --sname=sc_rssd
+perl %D%\rssd.pl --install --sname=sc_rssd
 perl %D%\watcherd.pl --install --sname=sc_watcherd
+
+sc config sc_polld start= demand
+sc config sc_rssd start= demand
