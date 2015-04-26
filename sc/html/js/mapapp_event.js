a0c12a501065fcde3850d944a8b83c536aafb577
Kuo-Wan Lin <klin@usgs.gov>
2015-04-26 16:07:39 -0600

    Post Data fix

--- sc/html/js/mapapp_event.js
+++ sc/html/js/mapapp_event.js
@@ -334,11 +334,11 @@ MAPAPP = (function() {
 			//sm_id = facility.shakemap_id + '-' + facility.shakemap_version;
 			//var dmg_url = '/scripts/damage.pl/from_id/'+sm_id+'?action=summary';
 			var dmg_url = '/scripts/r/damage/from_id/'+sm_id+'?action=summary';
+			var dmg_submit = {submit_data,  'action':'summary'};
 			if (facTypes != "ALL") {
-			  dmg_url += '?type=' + facTypes;
+			  dmg_submit.type= facTypes;
 			}
-			console.log(dmg_url);
-			$.post(dmg_url, submit_data, function(summary) {
+			$.post(dmg_url, dmg_submit, function(summary) {
 				var damage_summary = '<div class="progress">';
 				// Are there even any EQ to display?
 				if (summary.count > 0) {
@@ -480,7 +480,8 @@ MAPAPP = (function() {
 				//var local_url = '/scripts/shaking.pl/shaking_point/' + sm_id +
 				var local_url = '/scripts/r/shaking/shaking_point/' + sm_id +
 					'?longitude=' + event.latLng.lng() + '&latitude=' + event.latLng.lat();
-				$.post(local_url, submit_data, function(data) {
+		var local_sub = {submit_data, 'longitude': event.latLng.lng(), 'latitude': event.latLng.lat()};
+				$.post(local_url, local_sub, function(data) {
 		var infoContent = '<div  class="panel panel-warning"><div class="panel-heading text-center">' +
 			'<h4>No Information at Location (' + 
 			parseFloat(data.latitude).toFixed(4) + ',' + parseFloat(data.longitude).toFixed(4) + 
